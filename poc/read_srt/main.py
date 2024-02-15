a23d9fab5ea33cc0f4fa8b86d39bf8956346b6b2
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2024-02-15 00:11:34 -0800

    Add yt-dlp infinite retries, parse_srt

---
+++ poc/read_srt/main.py
@@ -0,0 +1,45 @@
+import json
+
+
+def time_to_seconds(time_str):
+    """
+    Convert a time string in the format 'HH:MM:SS,MS' to seconds.
+    """
+    hours, minutes, seconds_milliseconds = time_str.split(':')
+    seconds, milliseconds = seconds_milliseconds.split(',')
+    total_seconds = int(hours) * 3600 + int(minutes) * 60 + int(seconds) + int(milliseconds) / 1000
+    return total_seconds
+
+
+def parse_srt(srt_file_path):
+    with open(srt_file_path, 'r', encoding='utf-8') as file:
+        blocks = file.read().strip().split('\n\n')
+
+    subtitles = []
+    for block in blocks:
+        lines = block.split('\n')
+        if len(lines) >= 3:
+            subtitle_number = int(lines[0])
+            time_range = lines[1]
+            start_time_str, end_time_str = time_range.split(' --> ')
+            start_time = time_to_seconds(start_time_str)
+            end_time = time_to_seconds(end_time_str)
+            subtitle_text = '\n'.join(lines[2:])
+            subtitles.append({
+                # 'number': subtitle_number,
+                'start': start_time,
+                'end': end_time,
+                'text': subtitle_text
+            })
+
+    return subtitles
+
+
+# Example usage
+srt_file_path = 'ep3.srt'
+subtitles = parse_srt(srt_file_path)
+# for subtitle in subtitles:
+#     print(subtitle)
+
+with open('ep3.json', 'w', encoding='utf-8') as file:
+    json.dump(subtitles, file, ensure_ascii=False, indent=4)
--- scripts/commands.sh
+++ scripts/commands.sh
@@ -15,13 +15,28 @@ curl 'https://stream01.willfonk.com/live_playlist.m3u8?cid=BS296&r=FHD&ccode=JP&
 
 ffmpeg -i "https://stream01.willfonk.com/live_playlist.m3u8?cid=BS296&r=FHD&ccode=JP&m=d0:20:20:04:35:cc&t=0d6938cb3dcf4b79848bc1753a59daf1" -c copy -bsf:a aac_adtstoasc output.mp4
 
+vlc "https://stream01.willfonk.com/live_playlist.m3u8?cid=BS296&r=FHD&ccode=JP&m=d0:20:20:04:35:cc&t=0d6938cb3dcf4b79848bc1753a59daf1" --sout "#transcode{vcodec=h264,acodec=aac}:std{access=file,mux=mp4,dst='video.mp4'}"
+vlc "https://stream01.willfonk.com/live_playlist.m3u8?cid=BS296&r=FHD&ccode=JP&m=d0:20:20:04:35:cc&t=0d6938cb3dcf4b79848bc1753a59daf1" --sout=file/ps:go.mpg
+vlc "https://stream01.willfonk.com/live_playlist.m3u8?cid=BS296&r=FHD&ccode=JP&m=d0:20:20:04:35:cc&t=0d6938cb3dcf4b79848bc1753a59daf1" --sout="#std{access=file,mux=ps,dst=go.mp4}"
+vlc "https://stream01.willfonk.com/live_playlist.m3u8?cid=BS296&r=FHD&ccode=JP&m=d0:20:20:04:35:cc&t=0d6938cb3dcf4b79848bc1753a59daf1" --sout "#transcode{vcodec=h264,acodec=aac}:std{access=file,mux=mp4,dst='video.mp4'}"
+
+quicktime
+mp1v
+mpga
+
+
+ffmpeg -http_persistent 1 -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 60 -i "https://stream01.willfonk.com/live_playlist.m3u8?cid=BS296&r=FHD&ccode=JP&m=d0:20:20:04:35:cc&t=0d6938cb3dcf4b79848bc1753a59daf1" -c copy -bsf:a aac_adtstoasc "tbs feb 15.mp4"
+
 yt-dlp "https://stream01.willfonk.com/live_playlist.m3u8?cid=BS296&r=FHD&ccode=JP&m=d0:20:20:04:35:cc&t=0d6938cb3dcf4b79848bc1753a59daf1"
+
+# This is the one!
+yt-dlp --retries infinite --fragment-retries infinite --socket-timeout 9999 "https://stream01.willfonk.com/live_playlist.m3u8?cid=BS296&r=FHD&ccode=JP&m=d0:20:20:04:35:cc&t=0d6938cb3dcf4b79848bc1753a59daf1"
 
 yt-dlp "https://hls083.vipdracdn.net/streamhls2024/f89a76c7067505e25320415e0072de59/ep.2.v0.1707367415.720.m3u8"
 auto_subtitle improper2.mp4 -o improper2_en --language ja --output_srt
 
-yt-dlp "https://hls084.vipdracdn.net/streamhls2024/f89a76c7067505e25320415e0072de59/ep.3.v0.1707641216.720.m3u8" -o ep3.mp4
-auto_subtitle ep3.mp4 -o ep3_.srt --language ja --output_srt true --output_dir . --srt_only true
+yt-dlp "https://hls067.vipdracdn.net/streamhls2024/1d083add58333113de2545fa073818da/ep.8.v0.1680345949.480.m3u8" -o ep8.mp4
+auto_subtitle ep8.mp4 -o ep8_.srt --language ja --output_srt true --output_dir . --srt_only true
 
 #-rw-r--r-- 1 root root  289043635 Feb 13 17:15 'tbs breaking melon feb 14.mp4'
 #-rw-r--r-- 1 root root  116655255 Feb 13 14:07 'tbs eyeloveyou feb 13.mp4'
